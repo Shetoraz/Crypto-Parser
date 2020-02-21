c196d9a0bcc13ac4304fdd9f644e7da32502e96c
Shetora <shetora@holyman.local>
2020-02-21 18:00:41 +0300

    Cell design

--- CryptoParser/Controllers/ViewController.swift
+++ CryptoParser/Controllers/ViewController.swift
@@ -23,6 +23,8 @@ class ViewController: UIViewController {
     private func setupTable() {
         self.tableView.register(UINib(nibName: "CurrencyCustomTableViewCell", bundle: .main), forCellReuseIdentifier: "currencyCell")
         tableView.tableFooterView = UIView()
+        tableView.separatorColor = UIColor.clear
+
     }
 
     func addObserver() {
@@ -39,19 +41,39 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.model.currencies.count
+        return 1
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "currencyCell") as! CurrencyCustomTableViewCell
-        let name = self.model.currencies[indexPath.row].name ?? ""
-        let price = self.model.currencies[indexPath.row].currentPrice ?? 0.0
-        cell.setup(name, price: price)
+        let item = self.model.currencies[indexPath.section]
+        let name = item.name ?? ""
+        let price = item.currentPrice ?? 0.0
+        let change = item.priceChange24H ?? 0.0
+        cell.setup(name, price: price, change: change)
 
         return cell
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.model.currencies.count
+    }
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 7
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView()
+        header.backgroundColor = UIColor.clear
+        return header
     }
 }
--- CryptoParser/View/Custom Cell/CurrencyCustomTableViewCell.swift
+++ CryptoParser/View/Custom Cell/CurrencyCustomTableViewCell.swift
@@ -11,16 +11,28 @@ import UIKit
 class CurrencyCustomTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
-
     @IBOutlet weak var priceLabel: UILabel!
-
+    @IBOutlet weak var changeLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        layer.masksToBounds = false
+        layer.cornerRadius = 20
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowRadius = 3
+        layer.shadowColor = UIColor.lightGray.cgColor
+        layer.shadowOpacity = 0.3
+        layer.frame = frame
     }
 
-    func setup(_ name: String, price: Double) {
+    func setup(_ name: String, price: Double, change: Double) {
         self.nameLabel.text = name
-        self.priceLabel.text = String(price)
+        self.priceLabel.text = String(format: "%.3f", price)
+        self.changeLabel.text = String(format: "%.3f", change)
+        if change > 0 {
+            self.changeLabel.textColor = .systemGreen
+        } else {
+            self.changeLabel.textColor = .systemRed
+        }
     }
 }
